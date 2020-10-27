964c31d1036969caf2d042278ba7004aac21016f
alizarizqi <54764338+alizarizqi@users.noreply.github.com>
2020-10-27 11:20:19 +0700

    update button reset

--- app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
+++ app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
@@ -28,6 +28,13 @@ public class CounterViewModels extends ViewModel {
         scoreMutableLiveData.setValue(score);
         Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
     }
+    public void addButtonReset(){
+        score.setHomeScore(0);
+        score.setAwayScore(0);
+        scoreMutableLiveData.setValue(score);
+        Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
+        Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
+    }
 
     //TODO : Tambahkan Implementasi View Model Untuk Reset Point
 }
